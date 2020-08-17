b0272de8991badc2b50ec69c3c13e6d8b5e58241
bryn <thenazboz@gmail.com>
2020-08-17 11:18:00 +0100

    setup init, init the mysql db

---
+++ front-end/src/components/app/App.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { RootState } from '@App/store/reducers';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+
+interface AppProps {
+}
+
+interface AppState {
+}
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    height: 100vh;
+    background-color: #F9F9F9;
+    > div {
+      height: 100%;
+    }
+  }
+`;
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+class App extends React.Component<AppProps, AppState> {
+  public constructor(props: AppProps) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <>
+        <GlobalStyle />
+        <AppContainer>
+          {'hi'}
+        </AppContainer>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState, ownProps: object) => {};
+
+const mapDispatchToProps = (dispatch: Dispatch<RootState>) => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
